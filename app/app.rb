08c2c03f82528bb414b325481e93a4c3d917bd03
Patrick <p_donati@hotmail.com>
2016-03-22 12:41:07 +0000

    did things

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
+require_relative './models/link'
 
 class BookmarkManager < Sinatra::Base
 
   get '/' do
+    erb(:hello)
+  end
+
+
+  get '/links' do
     Link.create(link_address: "first_link", link_name: "this is the first link")
       puts Link.get(1)
     @link_address = "first_link"
--- spec/first_link_spec.rb
+++ spec/first_link_spec.rb
@@ -1,13 +1,19 @@
 feature 'links page' do
   Link.create(link_address: "first_link", link_name: "this is the first link")
-  
+
   scenario 'first page is actually loading' do
     visit '/'
     expect(page.status_code).to eq 200
   end
 
-  scenario 'first page has a default link' do
+  scenario 'first page has a button' do
     visit '/'
+    expect(page).to have_button "click here"
+  end
+
+  scenario 'link page has a default link' do
+    visit '/'
+    click_button "click here"
     expect(page).to have_link 'first_link'
   end
 
